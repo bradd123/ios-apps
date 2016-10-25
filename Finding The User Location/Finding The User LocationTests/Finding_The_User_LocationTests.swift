7039de1170e8a9de89df05652ead322656bb3ad6
brahma <brahma@gopassport.com>
2016-10-25 20:17:29 +0530

    add Find Your Location project

---
+++ Finding The User Location/Finding The User LocationTests/Finding_The_User_LocationTests.swift
@@ -0,0 +1,36 @@
+//
+//  Finding_The_User_LocationTests.swift
+//  Finding The User LocationTests
+//
+//  Created by Brahma Reddy Chilakala on 25/10/16.
+//  Copyright © 2016 Brahma Reddy Chilakala. All rights reserved.
+//
+
+import XCTest
+@testable import Finding_The_User_Location
+
+class Finding_The_User_LocationTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
