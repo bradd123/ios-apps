e31eb2402fdefc9380ab2f62c863b2402a49dd88
brahma <brahma@gopassport.com>
2016-11-28 13:32:08 +0530

    add changes in bond villains project

---
+++ ios-nd-uikit-cont.-swift-3/step5.5-bondVillains/BondVillains/VillainDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  VillainDetailViewController.swift
+//  BondVillains
+//
+//  Created by Brahma Reddy Chilakala on 28/11/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class VillainDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+}
