dd748b84e16525f5680a304213e29687ddc485e7
brahma <brahma@gopassport.com>
2016-11-30 12:24:34 +0530

    add BondVillains collection view

---
+++ ios-nd-uikit-cont.-swift-3/step6.1-bondVillains-noTabs/BondVillains/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  BondVillains
+//
+//  Created by Brahma Reddy Chilakala on 29/11/16.
+//  Copyright © 2016 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var villainImage: UIImageView!
+    
+    
+}
